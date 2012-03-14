b40fd50b3a2854e434a1806b8358cfc3a1729a4f
Joseph Rodriguez <jrodriguez@patientslikeme.com>
2012-03-14 18:55:25 -0400

    add easy method to Argonaut

--- lib/argonaut.rb
+++ lib/argonaut.rb
@@ -1,19 +1,19 @@
 require 'json'
 class Argonaut
   instance_methods.each {|m| undef_method(m) unless %w(__id__ __send__ to_json to_xml instance_eval nil? is_a? class).include?(m.to_s)}
-  
+
   def initialize(&blk)
     @hash = {}
     blk.call(self)
   end
-  
+
   def method_missing(key, *values)
     value = if block_given?
       Argonaut.new {|a| yield a}
     else
       values.size == 1 ? values.first : values
     end
-    
+
     if @hash[key].nil?
       @hash[key] = value
     elsif @hash[key].is_a? Array
@@ -22,7 +22,14 @@ class Argonaut
       @hash[key] = [@hash[key], value]
     end
   end
-  
+
+  # node.easy(object, :id, :created_at, :updated_at, :description)
+  def easy(obj, *methods)
+    methods.each do |mn|
+      @hash[mn] = obj.send(mn)
+    end
+  end
+
   def respond_to?(method) true end
   def to_xml(options = {}) @hash.to_xml(options) end
   def to_json() @hash.to_json end
